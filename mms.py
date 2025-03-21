f558c8cbb9004a5e39b3145c57574b3dfc58bc21
TJSothu <143737954+TJSothu@users.noreply.github.com>
2025-03-21 15:38:04 +0530

    created main body of the project
    updated and completed the memory managament simulator of the project

--- mms.py
+++ mms.py
@@ -8,6 +8,8 @@ class MemoryManagementSimulator:
         self.memory = [None] * self.num_frames  # Represents memory frames
         self.page_table = {}  # Maps pages to frames
         self.page_faults = 0
+        self.page_faults_over_time = []  # Track page faults over time
+        self.allocation_timeline = []  # Track allocation timeline
 
     def fifo_page_replacement(self, page):
         if page in self.page_table:
@@ -52,6 +54,8 @@ class MemoryManagementSimulator:
         self.fifo_queue = deque()  # For FIFO
         self.access_times = {}  # For LRU
         self.current_time = 0
+        self.page_faults_over_time = []  # Track page faults over time
+        self.allocation_timeline = []  # Track allocation timeline
 
         for process in processes:
             pages = self._get_pages_for_process(process)
@@ -61,8 +65,16 @@ class MemoryManagementSimulator:
                 elif algorithm == "LRU":
                     self.lru_page_replacement(page)
                 self.current_time += 1
+                self.page_faults_over_time.append(self.page_faults)
+                self.allocation_timeline.append((self.current_time, page, self.memory.copy()))
 
-        return self.page_faults
+        return {
+            "page_faults": self.page_faults,
+            "memory_allocation": self.memory,
+            "page_table": self.page_table,
+            "page_faults_over_time": self.page_faults_over_time,
+            "allocation_timeline": self.allocation_timeline
+        }
 
     def _get_pages_for_process(self, process):
         # Divide the process's memory requirements into pages
